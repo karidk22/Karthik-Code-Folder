84bf2a1f303d8dc102903c648e7bedeb0a0b0932
karidk22 <karthik.raghavun@rcsd.ca>
2024-09-16 08:57:38 -0600

    Working on ACSII Art

---
+++ 1- Lessons and Demos/Lesson4Demo/src/App.java
@@ -0,0 +1,12 @@
+// Project: Lesson 4 Demo
+// Name: Karthik Raghavun
+// Date: 13 September 2024
+// Description: Going over comments and special characters
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("How is your day going?");
+        System.out.println("Its cloudly with a chance of meatballs today!");
+    }
+}
