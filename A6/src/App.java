12140525486ed88781e40831b98cbc2aff994edc
karidk22 <karthik.raghavun@rcsd.ca>
2024-09-19 09:25:37 -0600

    Working on A6

---
+++ A6/src/App.java
@@ -0,0 +1,11 @@
+// A6 Variable Basics
+// Karthik Raghavun
+// 19 September 2024
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double chicken = 0.75;
+        double user_total = (4.75/chicken);
+        System.out.println("Since you have $4.75, you can get " + (int) user_total + " chicken wings.");
+    }
+}
