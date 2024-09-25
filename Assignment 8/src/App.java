32616d867f10505b0bb0fb886e6d084c3986076a
karidk22 <karthik.raghavun@rcsd.ca>
2024-09-25 09:46:13 -0600

    End of Wednesday

--- Assignment 8/src/App.java
+++ Assignment 8/src/App.java
@@ -5,24 +5,38 @@ public class App {
         System.out.println("Hello, World!");
          Scanner keyboard = new Scanner(System.in);
 
+      // Integer
+
       int intOne, intTwo; 
 
       System.out.print("Enter an integer :: ");
 
       intOne = keyboard.nextInt();
 
- 
-
       System.out.print("Enter an integer :: ");
 
       intTwo = keyboard.nextInt();
-
- 
 
       System.out.println("integer one = " + intOne);
 
       System.out.println("integer two = " + intTwo);
 
       System.out.println((String)("intOne + intTwo = ") + ((int)intOne + intTwo));
+
+      // Double
+
+      double doubleOne, doubleTwo;
+
+      System.out.println("Enter a double :: ");
+
+      doubleOne = keyboard.nextDouble();
+
+      System.out.println("Enter a double :: ");
+
+      doubleTwo = keyboard.nextDouble();
+
+      System.out.println("double one = " + doubleOne);
+      System.out.println("double two = " + doubleTwo);
+      System.out.println((String)("doubleOne + doubleTwo = ") + ((double)doubleOne + doubleTwo));
     }
 }
