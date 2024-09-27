f83c89585185286e2c2c5af324bda9a4d2b0db78
karidk22 <karthik.raghavun@rcsd.ca>
2024-09-27 09:02:55 -0600

    Finished assignment 7

--- Assignment 7/src/App.java
+++ Assignment 7/src/App.java
@@ -1,3 +1,9 @@
+// Assignment 7
+// Karthik Raghavun
+// 27 September 2024
+// Removing and adding words within a string using: .indexOf(), .length(), and .substring() 
+
+
 public class App {
     public static void main(String[] args) throws Exception 
     {
@@ -6,12 +12,16 @@ public class App {
 
         String message2 = "That was great - lol.";
         String target2 = "lol";
-
-        Integer message1pos = (message1.indexOf(message1));
-        Integer target1pos = (target1.length());
-
         
-                
+        // Modifying the output to disinclude "very"
+        int message1Apos = message1.indexOf(target1);
+        int message1Bpos = message1Apos + target1.length();
+        String newmessage1 = message1.substring(0, message1Apos)  + message1.substring(message1Bpos);
+        System.out.println(newmessage1);
         
+        // Removing "lol" and replacing it with "laugh out loud"
+        int lolpos = message2.indexOf(target2);
+        String newmessage2 = message2.substring(0, lolpos) + "laugh out loud" + message2.substring(lolpos + target2.length());
+        System.out.println(newmessage2);
     }
 }
