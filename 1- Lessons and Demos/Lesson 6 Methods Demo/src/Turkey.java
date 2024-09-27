df4a6a28f269daef41c866dfb980aef32f633bb6
karidk22 <karthik.raghavun@rcsd.ca>
2024-09-27 09:47:48 -0600

    End of Friday

---
+++ 1- Lessons and Demos/Lesson 6 Methods Demo/src/Turkey.java
@@ -0,0 +1,10 @@
+public class Turkey {
+    public void speak() {
+        System.out.println("Gobble Gobble");
+    }
+
+    public void sayName() {
+        System.out.println("big bird");
+        speak();
+    }
+}
---
+++ 1- Lessons and Demos/Lesson 6 Methods Demo/src/TurkeyRunner.java
@@ -0,0 +1,10 @@
+public class TurkeyRunner {
+    public static void main(String[] args) throws Exception {
+        Turkey bird = new Turkey();
+
+        bird.speak();
+        bird.sayName();
+        bird.speak();
+
+    }
+}
