cde99fe0b7697f5a61ef5109d6b2ab2290eb473e
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-01 09:13:27 -0600

    Tuesday
    Working on Assignment 8b

---
+++ Assignment 8b/src/KarthikA.java
@@ -0,0 +1,10 @@
+public class KarthikA {
+    public void speak() {
+        System.out.println("Hello!");
+    }
+    
+    public void karthik () {
+        System.out.println("I am from Saskatoon.");
+    }
+
+}
---
+++ Assignment 8b/src/KarthikB.java
@@ -0,0 +1,9 @@
+public class KarthikB {
+    public static void main(String[] args) throws Exception {
+        KarthikA old = new KarthikA();
+
+        old.speak();
+        old.karthik();
+        
+    }
+}
