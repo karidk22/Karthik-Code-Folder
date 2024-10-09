46bcb28f891990bb61fcd0c6fa55c2b1c7267c24
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-09 09:48:46 -0600

    Finished A10

--- Assignment 10/src/sumrunner.java
+++ Assignment 10/src/sumrunner.java
@@ -1,8 +1,19 @@
+// Assignment 10
+// Karthik Raghavun
+// 9 October 2024
+// Taking two numbers from the user inputted in a parameter and evaluting. 
+
 public class sumrunner {
     public static void main(String[] args) throws Exception {
 
         sum test = new sum();
-        test.setNums(2.0, 3.0);
+        test.setNums(2, 3);
+        test.sum();
+        test.printsum();
+        test.setNums(8, 4);
+        test.sum();
+        test.printsum();
+        test.setNums(100, 1);
         test.sum();
         test.printsum();
     }
