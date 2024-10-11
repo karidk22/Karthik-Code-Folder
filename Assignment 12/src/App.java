53b65663c55b04d747c69e4c4d66b5d10ce8aed8
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-11 09:48:00 -0600

    End of Friday

--- Assignment 12/src/App.java
+++ Assignment 12/src/App.java
@@ -1,5 +1,11 @@
+// Assignment 12
+// Karthik Raghavun
+// 11 October 2024
+// Converting fahrenheit to celcius and vice versa. 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Functions temp = new Functions();
+        temp.cTof(10);
+        temp.fToc(50);
     }
 }
--- Assignment 12/src/Functions.java
+++ Assignment 12/src/Functions.java
@@ -1,3 +1,22 @@
 public class Functions {
 
+    public int num1, num2;
+
+    public static void cTof(int a) {
+        double num1 = a;
+        double fahr = (((double)9/5) * num1 + 32);
+        System.out.println(fahr);
+        
+    }
+
+    public static void fToc(int b) {
+        double num2 = b;
+        double celc = (((double)5/9) * (num2 - 32));
+        System.out.println(celc);
+       
+    }
+   
+
+   
 }
+
