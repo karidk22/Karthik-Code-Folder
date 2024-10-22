939a6e4a4d9cf4a2ca9765dd1432ef3c9132493d
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-22 09:21:39 -0600

    Working on assignment 16

---
+++ Assignment 16/src/App.java
@@ -0,0 +1,27 @@
+// Assignment 16
+// Karthik Raghavun
+// 22 October 2024
+// Calculating the perimeter and area of a triangle with user input
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the triangle calculator!");
+        System.out.println("Please enter the length of side a: ");
+        int a = scanner.nextInt();
+        System.out.println("Please enter the length of side b: ");
+        int b = scanner.nextInt();
+        System.out.println("Please enter the length of side c: ");
+        int c = scanner.nextInt();
+
+        double Perimeter = a + b + c;
+        System.out.println("The perimter of the triangle is " + Perimeter);
+
+        double s = Perimeter / 2;
+        double Area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+        System.out.println("The area of the triangle is " + Math.round(Area));
+
+
+    }
+}
