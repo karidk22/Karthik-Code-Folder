345caf2d4a1233f099f6eb096a81f0c44a821428
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-24 21:21:38 -0600

    Update App.java

--- Assignment 17/src/App.java
+++ Assignment 17/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the distance you have traveled in Miles: ");
+        double distance = scanner.nextDouble();
+
+        System.out.println("Enter the time elapsed in hours: ");
+        double hours = scanner.nextInt();
+
+        System.out.println("Enter the time elapsed in minutes: ");
+        double minutes = scanner.nextInt();
+
+        MPHCalculations mphcalculations = new MPHCalculations(distance, hours, minutes);
+
+        double speed = mphcalculations.CalculateMPH();
+        System.out.println("The speed is " + Math.round(speed) + "MPH in " + distance + " miles in " + hours + " hour and " +  minutes + " minutes ");
+    
     }
 }
