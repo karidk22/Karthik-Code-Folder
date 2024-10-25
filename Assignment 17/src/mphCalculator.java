643557c6273fb5c98e74c175837ca97bbc9d1e06
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-24 21:22:09 -0600

    Update mphCalculator.java

--- Assignment 17/src/mphCalculator.java
+++ Assignment 17/src/mphCalculator.java
@@ -1,3 +1,20 @@
-public class mphCalculator {
+public class MPHCalculations {
+    private double distance;
+    private double hours;
+    private double minutes;
+
+    public MPHCalculations(double dist, double hrs, double mins) {
+        distance = dist;
+        hours = hrs;
+        minutes = mins;
+    }
+
+    public double CalculateMPH() {
+        double totalhrs = (double)(hours + (minutes / 60));
+        if (totalhrs == 0) {
+            return 0;
+        }
+        return distance / totalhrs;
+    }
 
 }
