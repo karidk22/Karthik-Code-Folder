9e4161025dde4aad0ad5faea96b7aaf40ea6622a
karidk22 <karthik.raghavun@rcsd.ca>
2024-10-28 08:56:40 -0600

    Finished leap year

--- Assignment 22/src/App.java
+++ Assignment 22/src/App.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a year to check if it is a leap year: ");
-        int x = scanner.nextInt();
+        System.out.print("Please enter a year to check if it is a leap year: ");
+        int year = scanner.nextInt();
+        LeapCalc leapcalc = new LeapCalc();
+        leapcalc.leapYear();
+
+        if (year % 4 == 0|| year % 400 ==0) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
         
         }
     }
--- Assignment 22/src/LeapCalc.java
+++ Assignment 22/src/LeapCalc.java
@@ -1,3 +1,10 @@
 public class LeapCalc {
+    private int year; 
 
+    public boolean leapYear() {
+        if (year % 4 == 0|| year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
 }
