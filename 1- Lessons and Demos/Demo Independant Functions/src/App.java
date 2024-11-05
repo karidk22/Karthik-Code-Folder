9d332bf5fb4b69034e4bb4af4ed71803a40ce2fb
karidk22 <karthik.raghavun@rcsd.ca>
2024-11-05 09:47:38 -0600

    Working on assignment 24

---
+++ 1- Lessons and Demos/Demo Independant Functions/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("4 double is " + numDouble(4));
+
+    }
+
+    public static int numDouble(int num) {
+        int ans = num * 2;
+
+        return ans;
+    }
+}
--- Assignment 24/src/App.java
+++ Assignment 24/src/App.java
@@ -1,14 +1,13 @@
+// Assignment 24
+// Karthik Raghavun
+// 5 November 2024
+// Even and Odd count
 import java.util.Scanner; 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        LoopStats loopStats = new LoopStats(0, 0);
-        int beg = scanner.nextInt();
-        int end = scanner.nextInt();
-        loopStats.setNums(0, 0);
-        loopStats.getEvenCount();
-        loopStats.getOddCount();
-        loopStats.getTotal();
+        LoopStats loopstats = new LoopStats(3, 5);
+        loopstats.getEvenCount();
+
     }
 }
