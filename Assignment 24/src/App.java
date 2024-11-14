11274e431074c9895da9c975e447e18783414a90
karidk22 <karthik.raghavun@rcsd.ca>
2024-11-14 09:15:22 -0600

    Finished Assignment 24

--- Assignment 24/src/App.java
+++ Assignment 24/src/App.java
@@ -7,16 +7,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in); // Calling the scanner function
 
         System.out.print("Enter a number: ");
-        int beg = scanner.nextInt();
+        int beg = scanner.nextInt(); // User inputs a number 
 
-        System.out.print("Enter a number: ");
-        int end = scanner.nextInt();
+        System.out.print("Enter a second number: ");
+        int end = scanner.nextInt(); // User inputs a second number 
 
-        LoopStats loopStats = new LoopStats(beg, end);
-        loopStats.Print();
+        LoopStats loopStats = new LoopStats(beg, end); // Calling method LoopStats
+        loopStats.Print(); // Calling the function in the method LoopStats
 
 
     }
